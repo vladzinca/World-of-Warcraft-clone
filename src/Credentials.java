6b9799db45b1a8376daacc7de4bacd873e075990
vladzinca <vlad_@VladsROG.localdomain>
2022-01-11 17:43:54 +0200

    Starts testing Credentials class

--- src/Credentials.java
+++ src/Credentials.java
@@ -1,2 +1,24 @@
 public class Credentials {
+    String email, password;
+
+    public Credentials(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
