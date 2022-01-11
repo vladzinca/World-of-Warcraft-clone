e15fab91ddb603bb096f4e71d9cdd01820a07304
vladzinca <vlad_@VladsROG.localdomain>
2022-01-11 17:52:41 +0200

    Successfully tests Credentials

--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,9 @@
 public class Test {
+    public static void main(String[] args) {
+        Credentials credentials = new Credentials("dragutzu_tao@gmail.com", "i_dont_laik_OOP");
+        System.out.println("Emailul si parola vechi: " + credentials.getEmail() + " si " + credentials.getPassword());
+        credentials.setEmail("vlad.zinca@stud.acs.upb.ro");
+        credentials.setPassword("i_laik_OOP");
+        System.out.println("Emailul si parola noi: " + credentials.getEmail() + " si " + credentials.getPassword());
+    }
 }
